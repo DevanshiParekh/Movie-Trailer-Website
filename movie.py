cb3020f5dbcfed397986a2048492af7c8c0529fa
Devanshi Parekh <devanshiparekh63@gmail.com>
2017-10-01 08:40:19 +0530

    comments added

--- movie.py
+++ movie.py
@@ -8,11 +8,18 @@ import webbrowser
 
 class Movie():
     def __init__(self,titles,storyline,poster,trailer):
-        self.title = titles
-        self.storyline = storyline
-        self.poster = poster
-        self.trailer = trailer
+       """
+       This constructor allows users to assign the title, storyline,
+       poster url and trailer url for respective movie instance.
+       """
+       self.title = titles
+       self.storyline = storyline
+       self.poster = poster
+       self.trailer = trailer
         
     def getTrailer(self):
+        """
+        This method allows user to open the trailer in the webbrowser.
+        """
         webbrowser.open(self.trailer)
         
